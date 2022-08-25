ac3cf6ff6030f2dc2dd7afaf7c74dcb21a58fb30
KomovTA <komovta3@gmail.com>
2022-08-25 09:54:48 +0300

    start work

--- Timofey 1.py
+++ Timofey 1.py
@@ -1,2 +1,15 @@
-while True:
-    print('defwwdvwvw')
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://techcrunch.com/2022/08/24/privado-is-grammarly-for-code-privacy-issues/'
+response = requests.get(url)
+response.raise_for_status()
+soup = BeautifulSoup(response.text, 'lxml')
+
+images = []
+
+images_code = soup.find_all('img', class_="article__featured-image")
+for image_code in images_code:
+    images.append(image_code)
+    print(images)
+#print(soup.find_all('<time>', class_="full-date-time"))
