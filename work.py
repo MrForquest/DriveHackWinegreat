ac5e6b7447f3451ad6f13d27efe87029bfa837b3
KomovTA <komovta3@gmail.com>
2022-08-26 00:49:31 +0300

    done function to parsing next pages for 4 sites

--- work.py
+++ work.py
@@ -1,12 +1,31 @@
 from Alex_part import get_links_from_main_link
 from Tim_part import get_info_from_link
+import time
 alll = []
 
-lin = ['https://techcrunch.com/category/startups/', 'https://techstartups.com/category/startups/',
+links = ['https://techcrunch.com/category/startups/', 'https://techstartups.com/category/startups/',
        'https://www.eu-startups.com/', 'https://startupnews.com.au/category/news/',
        'https://www.techstars.com/newsroom', 'https://inc42.com/buzz/']
+diffrent_cites_types = {'rss': ['techcrunch', 'techstartups', 'eu-startups', 'startupnews'],
+                        'html': ['techstars', 'inc42']}
 
-for i in lin:
-    for j in get_links_from_main_link(i):
-        print(get_info_from_link(j))
-print(alll, end='\n')
+def get_links_from_diff_pages(link):
+    if link == 'https://techcrunch.com/category/startups/' or link == 'https://inc42.com/buzz/':
+        return get_links_from_main_link(link)
+    elif link == 'https://techstartups.com/category/startups/' or link == 'https://startupnews.com.au/category/news/' \
+            or link == 'https://www.eu-startups.com/':
+        pages = []
+        for page in range(5):
+            if page == 0:
+                pages += get_links_from_main_link(link)
+            else:
+                pages += get_links_from_main_link(link + f"page/{page}/")
+        return pages
+    elif link == 'https://www.techstars.com/newsroom':
+        pages= []
+        for page in range(10):
+            if page == 0:
+                pages += get_links_from_main_link(link)
+            else:
+                pages += get_links_from_main_link(link + f"?page={page}")
+        return pages
