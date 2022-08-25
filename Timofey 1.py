3fbf4095caa42b9cb75a376e45e2fa34b02d1115
KomovTA <komovta3@gmail.com>
2022-08-25 13:25:20 +0300

    add para new functions

--- Timofey 1.py
+++ Timofey 1.py
@@ -1,15 +1,44 @@
+import datetime as dt
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://techcrunch.com/2022/08/24/privado-is-grammarly-for-code-privacy-issues/'
-response = requests.get(url)
-response.raise_for_status()
-soup = BeautifulSoup(response.text, 'lxml')
 
-images = []
+def get_info_from_techcrunch(link):
+    url = link
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.content, 'lxml')
+    images = [soup.find('img')['src']]
+    # images = list(map(lambda x: x['src'], soup.find_all('img')))
+    text = ' '.join(' '.join(list(map(lambda x: x.text, soup.find_all('p')))).split('\xa0'))
+    article = ' '.join(' '.join(list(map(lambda x: x.text, soup.find_all('h1')))).split('\xa0'))
+    date = soup.find('time').text
+    return link, article, text, images, date
 
-images_code = soup.find_all('img', class_="article__featured-image")
-for image_code in images_code:
-    images.append(image_code)
-    print(images)
-#print(soup.find_all('<time>', class_="full-date-time"))
+def get_info_from_cnbc(link):
+    url = link
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    images = [soup.find('img')['src']]
+    # images = list(map(lambda x: x['src'], soup.find_all('img')))
+    text = ' '.join(list(map(lambda x: x.text, soup.find_all('p'))))
+    article = ' '.join(list(map(lambda x: x.text, soup.find_all('h1'))))
+    date = soup.find('time').text
+
+    return link, article, text, images, date
+
+def get_info_from_techstartups(link):
+    url = link
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    images = [soup.find('img')['src']]
+    # images = list(map(lambda x: x['src'], soup.find_all('img')))
+    text = ' '.join(' '.join(list(map(lambda x: x.text, soup.find_all('p')))).split('\xa0'))
+    article = ' '.join(' '.join(list(map(lambda x: x.text, soup.find_all('h1')))).split('\xa0'))
+    date = soup.find(class_='post_info_date').text.strip()
+    return link, article, text, images, date
+
+print(get_info_from_techcrunch('https://techcrunch.com/2022/08/25/egypts-subsbase-raises-2-4m-for-its-subscription-and-recurring-revenue-management-platform/'), end='\n')
+#print(get_info_from_techstartups('https://techstartups.com/2022/08/24/zebox-america-announces-cohort-9-new-startups-logistics-supply-chain-accelerator-program/'), end='\n')
---
+++ testing.py
@@ -0,0 +1,2 @@
+a = 'ZEBOX America announces cohort of 9 new startups for its\xa0logistics and supply chain accelerator program'
+print(a.split('\'))
